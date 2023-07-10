b090964cf3d807546ce845a0c5ac4a8a34cfdc01
Tomas <tomush4@gmail.com>
2023-07-10 10:15:42 +0100

    Add additional words to filter.

--- WordCounter/Counter.cs
+++ WordCounter/Counter.cs
@@ -7,8 +7,9 @@ namespace WordCounter
 {
     public static class Counter
     {
-        private static readonly string[] FILTER_WORDS = { "out", "for", "how", "the", "not", "who", "and", "her", "his", "you", "was", "from", "with", "after", "over", "into", "bbc" };
+        private static readonly string[] FILTER_WORDS = { "but", "your", "can", "out", "for", "how", "the", "not", "who", "and", "her", "his", "you", "was", "from", "with", "after", "over", "into", "bbc" };
         private const int WORD_MIN_LENGTH = 3;
+        private const int WORD_MAX_LENGTH = 80;
         private static readonly Dictionary<string, string> NEWS_OUTLETS_URLS = new Dictionary<string, string>
         {
             {"bbc","http://feeds.bbci.co.uk/news/uk/rss.xml"},
@@ -93,7 +94,7 @@ namespace WordCounter
         {
             List<string> filtered = (from string word in words
                                      let lowerCaseWord = word.ToLower()
-                                     where !filterWords.Contains(lowerCaseWord) && lowerCaseWord.Length >= wordMinLength
+                                     where !filterWords.Contains(lowerCaseWord) && lowerCaseWord.Length >= wordMinLength && lowerCaseWord.Length <= WORD_MAX_LENGTH
                                      select lowerCaseWord).ToList();
             return filtered.ToArray();
         }
--- WordFrequencyAPI/Program.cs
+++ WordFrequencyAPI/Program.cs
@@ -1,7 +1,6 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
